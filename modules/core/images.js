7873835caf58ba1dd1f2fe30fad9606981abc269
ffamilyfriendly <jonathan.hermin@skola.sala.se>
2021-01-15 13:32:27 +0100

    added profile pics

--- modules/core/images.js
+++ modules/core/images.js
@@ -2,5 +2,8 @@ module.exports = {
 	"default":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/%D8%A7%D8%AD%D9%85%D8%AF_%D8%B5%D8%A8%D8%B1%D9%8A.JPG/447px-%D8%A7%D8%AD%D9%85%D8%AF_%D8%B5%D8%A8%D8%B1%D9%8A.JPG",
 	"obama":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/480px-President_Barack_Obama.jpg",
 	"mike_pence":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Mike_Pence_official_Vice_Presidential_portrait.jpg/96px-Mike_Pence_official_Vice_Presidential_portrait.jpg",
-	"spongebob":"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Solingen-Ohligs_Spongebob_in_der_Fu%C3%9Fg%C3%A4ngerzone_-_panoramio.jpg/90px-Solingen-Ohligs_Spongebob_in_der_Fu%C3%9Fg%C3%A4ngerzone_-_panoramio.jpg"
+	"spongebob":"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Solingen-Ohligs_Spongebob_in_der_Fu%C3%9Fg%C3%A4ngerzone_-_panoramio.jpg/90px-Solingen-Ohligs_Spongebob_in_der_Fu%C3%9Fg%C3%A4ngerzone_-_panoramio.jpg",
+	"fortnite_ninja":"https://www.prosettings.com/site/wp-content/uploads/ninja-fortnite-settings.jpg",
+	"sellout":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/PewDiePie_at_PAX_2015_crop.jpg/450px-PewDiePie_at_PAX_2015_crop.jpg",
+	"churchill":"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Winston_Churchill_cph.3a49758_%28cropped%29.jpg/800px-Winston_Churchill_cph.3a49758_%28cropped%29.jpg"
 }
